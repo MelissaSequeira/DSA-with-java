bffdd6facbde3bf846fe0a57717e5b56dcf2784f
MelissaSequeira <melissasequeira572@gmail.com>
2025-06-26 13:56:29 +0530

    Rotate array k times

--- StriverA-z/RotateKtimes.java
+++ StriverA-z/RotateKtimes.java
@@ -2,26 +2,55 @@ import java.util.Scanner;
 
 public class RotateKtimes {
     public static int[] rotate(int[] nums, int k) {
-        if (nums.length != 1) {
-            while (k != 0) {
-                int temp = nums[nums.length - 1];
-                for (int i = nums.length - 2; i >= 0; i--) {
-                    nums[i + 1] = nums[i];
-                }
-                nums[0] = temp;
-                k--;
-            }
-            return nums;
-        } else {
-            return nums;
+        k = k % nums.length;
+        // if(nums.length==1){
+
+        // }
+        // else{
+        // while(k!=0){
+        // int temp=nums[nums.length-1];
+        // for(int i=nums.length-2;i>=0;i--){
+        // nums[i+1]=nums[i];
+        // }
+        // nums[0]=temp;
+        // k--;
+        // }
+        // }
+        int l = 0;
+        int r = nums.length - 1;
+        while (l < r) {
+            int temp = nums[r];
+            nums[r] = nums[l];
+            nums[l] = temp;
+            l++;
+            r--;
         }
+        l = 0;
+        r = k - 1;
+        while (l < r) {
+            int temp = nums[r];
+            nums[r] = nums[l];
+            nums[l] = temp;
+            l++;
+            r--;
+        }
+        l = k;
+        r = nums.length - 1;
+        while (l < r) {
+            int temp = nums[r];
+            nums[r] = nums[l];
+            nums[l] = temp;
+            l++;
+            r--;
+        }
+        return nums;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int k = sc.nextInt();
         int n = sc.nextInt();
-        
+
         int[] nums = new int[n];
         for (int i = 0; i <= n - 1; i++) {
             nums[i] = sc.nextInt(); // ✅ Correct
